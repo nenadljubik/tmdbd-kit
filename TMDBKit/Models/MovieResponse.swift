d03c71f05f2fdaef1f97a6b4a82cb4df901b5a8d
Nenad Ljubik <nenadljubik@hotmail.com>
2025-12-29 03:36:23 +0100

    Conformed Movie to Hashable

--- TMDBKit/Models/MovieResponse.swift
+++ TMDBKit/Models/MovieResponse.swift
@@ -77,3 +77,13 @@ public struct Movie: Codable, Identifiable {
         )
     }
 }
+
+extension Movie: Hashable {
+    public static func == (lhs: Movie, rhs: Movie) -> Bool {
+        lhs.id == rhs.id
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
